826ede16a9bec858a5ecc40055392e4b3d3542d6
Carlos Velazquez <skamateria@gmail.com>
2019-06-04 10:00:55 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,7 +3,7 @@ def countdown(num)
     puts "#{num} SECOND(S)!"
     num -= 1
   end
-  puts "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
 end
 
 
@@ -11,8 +11,8 @@ end
 def countdown_with_sleep(num)
   while num > 0
     puts "#{num} SECOND(S)!"
-    sleep 1
+    sleep(1)
     num -= 1
   end
-  puts "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
 end
